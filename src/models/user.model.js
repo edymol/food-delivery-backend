8aceb18ccbbaa62b5c980698b64860c476dd7189
edymol Github <edymolinap@gmail.com>
2023-09-24 08:03:02 -0700

    removing password and __v from get request

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,22 @@
 const { Schema } = require('mongoose');
 const mongoose = require('mongoose');
 
-
 const userSchema = new Schema({
     firstName: {
         type: String,
         required: true
     },
     lastName: {
-        type:String,
+        type: String,
         required: true
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true
     },
     password: {
-        type:String,
+        type: String,
         required: true
     },
     address: {
@@ -27,7 +26,15 @@ const userSchema = new Schema({
     phone: {
         type: String,
         required: true
-    },
+    }
+}, {
+    toJSON: {
+        transform: (doc, ret) => {
+            delete ret.password;
+            delete ret.__v;
+            return ret;
+        }
+    }
 });
 
 const User = mongoose.model('User', userSchema);
