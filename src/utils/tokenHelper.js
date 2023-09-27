f2a8ebcb5844cacc3a3988bb806f8992b7911cb0
edymol Github <edymolinap@gmail.com>
2023-09-27 15:32:38 -0700

    finished token creation and added .env file

--- src/utils/tokenHelper.js
+++ src/utils/tokenHelper.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 // This function will generate a token for the user.
 
 const generateToken = (email) => { //if you want to add more data to the token, you can add it here as a parameter and then add it to the object in the jwt.sign function. email, firstName, lastName, etc.
     const token = jwt.sign(
         { email }, // this is the payload add the same parameters, firstName, lastName, etc. here.
-        "yOuR sEcReT kEy", 
+        process.env.JWT_SECRET, 
         { expiresIn: '3d' }
     );
     return token;
@@ -13,7 +14,7 @@ const generateToken = (email) => { //if you want to add more data to the token, 
 
 // This function will decode the token.
 const tokenDecoder = (token) => {
-    const tokenDecoded = jwt.verify(token, "yOuR sEcReT kEy");
+    const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
     return tokenDecoded;
 };
 module.exports = {
